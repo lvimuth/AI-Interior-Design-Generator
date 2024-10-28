ccafed3416a824c69fd7fc4b397e940d70eade53
Lakshitha Vimuth <lvimuthfb@gmail.com>
2024-10-28 21:24:01 +0400

    clerk authentication

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,11 +1,14 @@
+import { ClerkProvider } from "@clerk/nextjs";
 import "./globals.css";
 import { Outfit } from "next/font/google";
 
 const outfit = Outfit({ subsets: ["latin"] });
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={outfit.className}>{children}</body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={outfit.className}>{children}</body>
+      </html>
+    </ClerkProvider>
   );
 }
